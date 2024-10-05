8520e9825e03044c80ed290682345735cd6591e4
kingbri <bdashore3@proton.me>
2024-10-05 13:00:41 -0400

    Tree: Update for 2024 conventions
    - Top-level await now works
    - Use NodeNext for module resolution and ESNext for targets
    - Update eslint
    - Update all dependencies
    - Use pnpm as the default package manager
    
    Signed-off-by: kingbri <bdashore3@proton.me>

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,5 @@
-// Remove the async call if you don't require async code
-try {
-    await main();
-} catch (err) {
-    console.log(`Uncaught exception: \n\n${err}`);
-}
-
 async function main() {
     console.log('This is some cool async stuff');
 }
+
+await main();
